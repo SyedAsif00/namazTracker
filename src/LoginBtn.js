f636cc8c01c00971c768999053c07ac0946d9475
Syed Asifullah <syedasifullah00@gmail.com>
2024-09-10 12:01:01 +0500

    user persistence after reload

--- src/LoginBtn.js
+++ src/LoginBtn.js
@@ -1,9 +1,9 @@
 // src/LoginBtn.js
 import React from "react";
-import { Button } from "antd"; // Import Ant Design button
+import { Button, message } from "antd"; // Import Ant Design button
 import { auth } from "./firebase.config"; // Use already initialized auth instance
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore, doc, setDoc } from "firebase/firestore";
+import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
 import { app } from "./firebase.config"; // Import the app instance
 
 const db = getFirestore(app); // Initialize Firestore
@@ -15,8 +15,11 @@ const GoogleLoginButton = ({ setUser }) => {
     signInWithPopup(auth, provider)
       .then(async (result) => {
         const user = result.user;
+        const userDocRef = doc(db, "users", user.uid);
+        const userDocSnap = await getDoc(userDocRef);
         setUser(user);
 
+        if (userDocSnap) return message.info("welcome back.");
         // Save user data to Firestore
         await setDoc(doc(db, "users", user.uid), {
           uid: user.uid,
